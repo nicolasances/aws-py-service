eb2eedde7990043ffb8bd0dfc95fc3a9fb57a03b
nicolasances <nicolas.matteazzi@gmail.com>
2024-05-24 20:48:29 +0200

    Using new TotoAPIController 1.1.0 with AWS support

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
-from TotoConfig import TotoConfig
+from totoapicontroller.model.TotoConfig import TotoConfig
+from totoapicontroller.model.TotoConfig import CloudProvider
 from totoapicontroller.model.singleton import singleton
 
 @singleton
 class Config(TotoConfig): 
     
     def __init__(self):
-        super().__init__()
+        super().__init__(cloud_provider=CloudProvider.AWS)
         
         self.logger.log("INIT", "Configuration loaded!")
         
